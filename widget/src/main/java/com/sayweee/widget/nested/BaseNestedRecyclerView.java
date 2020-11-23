7777d83e5cf4ca6c731085fece149e55f8dbb15c
winds <heardown@163.com>
2020-11-23 16:05:31 +0800

    sync

--- widget/src/main/java/com/sayweee/widget/nested/BaseNestedRecyclerView.java
+++ widget/src/main/java/com/sayweee/widget/nested/BaseNestedRecyclerView.java
@@ -1,4 +1,4 @@
-package com.sayweee.widget.nest;
+package com.sayweee.widget.nested;
 
 import android.content.Context;
 import android.util.AttributeSet;
--- widget/src/main/java/com/sayweee/widget/nested/ParentRecyclerView.java
+++ widget/src/main/java/com/sayweee/widget/nested/ParentRecyclerView.java
@@ -1,8 +1,7 @@
-package com.sayweee.widget.nest;
+package com.sayweee.widget.nested;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
